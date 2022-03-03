a13c1ca48f2fc799b9ca8fbc13bc9fe5005e06a6
DinoSahr <kanjama7@yahoo.com>
2022-03-02 21:20:49 -0600

    Version 1.1.1 - Configured Static Files URL

--- DinoSahrTipCalculator/settings.py
+++ DinoSahrTipCalculator/settings.py
@@ -137,9 +137,13 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
